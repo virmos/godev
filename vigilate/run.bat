f8e470bd634f5450b94017b13bb8b6105408d488
virmos <hadonggiang1810@gmail.com>
2022-09-06 23:26:10 +0700

    setup microservices

--- vigilate/run.bat
+++ vigilate/run.bat
@@ -2,5 +2,6 @@ cd ipe
 start /min cmd /c ipe.exe &
 cd ..
 go build -o vigilate.exe ./cmd/web/.
-docker compose up -d
-vigilate -dbuser='postgres' -dbpass='qwerqwer' -pusherHost="localhost" -pusherPort="4001" -pusherSecret="123abc" -pusherKey="abc123" -pusherSecure=false -pusherApp="1" -db="vigilate" -redisHost="localhost:6379" -redisPrefix="vigilate"
+@REM docker compose up -d
+soda migrate
+vigilate -dbuser='postgres' -dbpass='qwerqwer' -pusherHost="localhost" -pusherPort="4001" -pusherSecret="123abc" -pusherKey="abc123" -pusherSecure=false -pusherApp="1" -db="temp" -redisHost="localhost:6379" -redisPrefix="vigilate"
