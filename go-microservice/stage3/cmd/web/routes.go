21fc1a149c6baa0c5ba923382b780f591ba517aa
virmos <hadonggiang1810@gmail.com>
2022-09-08 23:19:01 +0700

    authentication

---
+++ go-microservice/stage3/cmd/web/routes.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+	mux.Use(SessionLoad)
+
+	mux.Get("/", app.Home)
+
+	mux.Get("/virtual-terminal", app.VirtualTerminal)
+	mux.Post("/virtual-terminal-payment-succeeded", app.VirtualTerminalPaymentSucceeded)
+	mux.Get("/virtual-terminal-receipt", app.VirtualTerminalReceipt)
+
+	mux.Get("/widget/{id}", app.ChargeOnce)
+	mux.Post("/payment-succeeded", app.PaymentSucceeded)
+	mux.Get("/receipt", app.Receipt)
+
+	mux.Get("/plans/bronze", app.BronzePlan)
+	mux.Get("/receipt/bronze", app.BronzePlanReceipt)
+
+	// auth routes
+	mux.Get("/login", app.LoginPage)
+
+	fileServer := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
+	return mux
+}
